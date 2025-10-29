f6caabeff11094c2706b99e4d382d3bcd5c5dda7
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-10-30 08:46:18 +0900

    Title: 구구단 만들기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251030/구구단 만들기/print-multiplication-table.py
+++ 251030/구구단 만들기/print-multiplication-table.py
@@ -5,9 +5,11 @@ for i in range(b,a-1,-1):
     if i % 2 ==0:
         lst.append(i)
 
+k = min(lst)
+
 for i in range(1,10):
     for j in lst:
         print(f'{j} * {i} = {j*i}',end=' ')
-        if j > 2:
+        if j > k:
             print('/',end=' ')
     print()

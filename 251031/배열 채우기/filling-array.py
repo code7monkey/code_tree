cd80bf41276ff01061754f3a2b7687d5638617a7
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-10-31 11:17:20 +0900

    Title: 배열 채우기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251031/배열 채우기/filling-array.py
+++ 251031/배열 채우기/filling-array.py
@@ -1,4 +1,9 @@
-lst = list(map(int,input().split()))
+p_lst = list(map(int,input().split()))
+lst= []
+for i in p_lst:
+    lst.append(i)
+    if i == 0:
+        break
 
 n = len(lst)
 for i in range(n-1,-1,-1):

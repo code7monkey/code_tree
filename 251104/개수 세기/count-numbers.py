2109905620a28c6d26a8a90f6f56af80fd6e367e
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-04 14:53:55 +0900

    Title: 개수 세기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251104/개수 세기/count-numbers.py
+++ 251104/개수 세기/count-numbers.py
@@ -1,7 +1,10 @@
 n, m =map(int,input().split())
 lst = list(map(int,input().split()))
 idx = -1
+cnt = 0
 
 for i in range(n):
     if lst[i] == m:
-        print(i+1)
+        cnt += 1
+        
+print(cnt)

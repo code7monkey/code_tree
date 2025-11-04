d74b5f4ced927b35b3c05c4e5fab2f7514236218
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-04 10:01:07 +0900

    Title: 십의 자리 숫자, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251104/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,17 @@
+lst = list(map(int,input().split()))
+
+arr = [0]*9
+lst2 = []
+for i in lst:
+    if i < 10:
+        continue
+    else: 
+        lst2.append(i//10)
+
+for i in lst2:
+    arr[i-1] += 1
+
+i = 0
+for j in arr: 
+    i += 1
+    print(f'{i} - {j}')

673986bf0955f99f84dd46513089d4eba3e9d213
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-05 09:11:57 +0900

    Title: 연속부분수열일까, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251105/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,26 @@
+na, nb = map(int,input().split())
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+start = b[0]
+
+idx = -1
+for i in range(na):
+    if a[i] == start:
+        idx = i
+        break
+
+a_chk = a[idx:idx+nb]
+
+cnt = 0
+
+for i,j in zip(a_chk,b):
+    if i != j:
+        print('No')
+        break
+    else:
+        cnt +=1
+
+if cnt == nb:
+    print('Yes')

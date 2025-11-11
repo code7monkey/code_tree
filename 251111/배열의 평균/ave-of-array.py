d770ec994e7d4a1ef281c87d3167362be58707e3
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-11 09:14:39 +0900

    Title: 배열의 평균, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251111/배열의 평균/ave-of-array.py
+++ 251111/배열의 평균/ave-of-array.py
@@ -1,11 +1,11 @@
 lst1 = list(map(int,input().split()))
 lst2 = list(map(int,input().split()))
 
-print(sum(lst1)/4, end=' ')
-print(sum(lst2)/4)
+print(round(sum(lst1)/4,1), end=' ')
+print(round(sum(lst2)/4,1))
 
 for i in range(4):
-    print((lst1[i]+lst2[i])/2,end=' ')
+    print(round((lst1[i]+lst2[i])/2,1),end=' ')
 print()
 
-print((sum(lst1)+sum(lst2))/8)
+print(round((sum(lst1)+sum(lst2))/8,1))

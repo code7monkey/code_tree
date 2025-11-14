c0b9558e27b26e12e8eb40b39605435734b974b4
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-14 09:28:18 +0900

    Title: 격자 모양 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251114/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,13 @@
+n,m = map(int,input().split())
+
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for _ in range(m):
+    i, j = map(int,input().split())
+    arr[i-1][j-1] = i * j
+
+for row in arr:
+    print(*row)

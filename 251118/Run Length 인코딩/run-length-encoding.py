e7dc9d17d2844a5c932a74369c3b7b4fd7364104
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-18 08:50:51 +0900

    Title: Run Length 인코딩, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251118/Run Length 인코딩/run-length-encoding.py
+++ 251118/Run Length 인코딩/run-length-encoding.py
@@ -9,8 +9,11 @@ for i in range(n-1):
     if i == n-2:
         if aa[i] == aa[i+1]:
             cnt += 1
-        lst.append(aa[i])
-        lst.append(cnt)
+            lst.append(aa[i])
+            lst.append(cnt)
+        else:
+            lst.append(aa[i+1])
+            lst.append(1)
     elif aa[i] == aa[i+1]:
        cnt += 1
     else:

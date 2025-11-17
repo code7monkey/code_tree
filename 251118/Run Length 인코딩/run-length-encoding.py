711b1e52f7a299a14db49c21896e57ce2e5d4694
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-18 08:48:56 +0900

    Title: Run Length 인코딩, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251118/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,29 @@
+aa = input()
+
+n = len(aa)
+
+cnt = 1
+lst = []
+
+for i in range(n-1):
+    if i == n-2:
+        if aa[i] == aa[i+1]:
+            cnt += 1
+        lst.append(aa[i])
+        lst.append(cnt)
+    elif aa[i] == aa[i+1]:
+       cnt += 1
+    else:
+        lst.append(aa[i])
+        lst.append(cnt)
+        cnt = 1 
+
+ll = 0
+
+for i in lst:
+    ll += len(str(i))
+
+print(ll)
+
+for i in lst:
+    print(i,end='')

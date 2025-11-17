f76a219e8602876b9ca85a9f4970cc6f705620f9
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-18 08:56:08 +0900

    Title: Run Length 인코딩, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251118/Run Length 인코딩/run-length-encoding.py
+++ 251118/Run Length 인코딩/run-length-encoding.py
@@ -4,31 +4,31 @@ n = len(aa)
 
 cnt = 1
 lst = []
-
-for i in range(n-1):
-    if i == n-2:
-        if aa[i] == aa[i+1]:
+if n >= 2:
+    for i in range(n-1):
+        if i == n-2:
+            if aa[i] == aa[i+1]:
+                cnt += 1
+                lst.append(aa[i])
+                lst.append(cnt)
+            else:
+                lst.append(aa[i])
+                lst.append(cnt)
+                lst.append(aa[i+1])
+                lst.append(1)
+        elif aa[i] == aa[i+1]:
             cnt += 1
-            lst.append(aa[i])
-            lst.append(cnt)
         else:
             lst.append(aa[i])
             lst.append(cnt)
-            lst.append(aa[i+1])
-            lst.append(1)
-    elif aa[i] == aa[i+1]:
-       cnt += 1
-    else:
-        lst.append(aa[i])
-        lst.append(cnt)
-        cnt = 1 
-
-ll = 0
-
-for i in lst:
-    ll += len(str(i))
-
-print(ll)
-
-for i in lst:
-    print(i,end='')
+            cnt = 1
+    ll = 0
+    for i in lst:
+        ll += len(str(i))
+    print(ll)
+    for i in lst:
+        print(i,end='')
+else:
+    print(2)
+    print(aa,end='')
+    print(1)

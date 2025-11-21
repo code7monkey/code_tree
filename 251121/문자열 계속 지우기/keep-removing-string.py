9d22996565ea4f27cdd17361ba05a59812df6567
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-11-21 15:12:00 +0900

    Title: 문자열 계속 지우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251121/문자열 계속 지우기/keep-removing-string.py
+++ 251121/문자열 계속 지우기/keep-removing-string.py
@@ -1,22 +1,9 @@
 a = input()
 b = input()
-
-lst = list(a)
 n = len(b)
 
-b_list = list(b)
+while a.find(b) != -1:
+    idx = a.find(b)
+    a = a[:idx] + a[idx+n:]
 
-while True:
-    found = False
-    i = 0
-    while i <= len(lst) - n:
-        if lst[i:i+n] == b_list:
-            lst = lst[:i] + lst[i+n:]
-            found = True
-            break
-        i += 1
-
-    if not found:  # 더 이상 제거할게 없으면 종료
-        break
-
-print(''.join(lst))
+print(a)

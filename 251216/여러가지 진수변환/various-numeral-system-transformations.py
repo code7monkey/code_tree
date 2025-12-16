dd79c71ba0e4e9672cebe8985ce3b47451c62fbf
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-12-16 14:35:31 +0900

    Title: 여러가지 진수변환, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251216/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,16 @@
+n, b = map(int, input().split())
+
+# Please write your code here.
+digits = []
+
+while True:
+    if n < b:
+        digits.append(n)
+        break
+
+    digits.append(n % b)
+    n //= b
+
+# print binary number
+for digit in digits[::-1]:
+    print(digit, end="")

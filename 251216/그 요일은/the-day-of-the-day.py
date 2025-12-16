1be7385cef09e583e1767fc56b40687a38a989c8
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-12-16 13:48:05 +0900

    Title: 그 요일은, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251216/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+month , day = m1, d1
+elapsed_time = 0
+
+while True:
+    if month == m2 and day == d2:
+        break
+
+    elapsed_time += 1
+    day += 1
+
+    if day > num_of_days[month]:
+        month += 1
+        day = 1
+
+print(1+ (elapsed_time//7))

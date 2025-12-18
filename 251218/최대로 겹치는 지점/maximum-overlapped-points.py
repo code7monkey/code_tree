a8c213704ce8056fc8e8cb35ca3000c02612b560
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-12-18 10:01:29 +0900

    Title: 최대로 겹치는 지점, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 251218/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 251218/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -4,7 +4,7 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 arr = [0] * 100
 for a,b in segments:
-    for i in range(a,b+1):
+    for i in range(a-1,b):
         arr[i] += 1
 
 print(max(arr))

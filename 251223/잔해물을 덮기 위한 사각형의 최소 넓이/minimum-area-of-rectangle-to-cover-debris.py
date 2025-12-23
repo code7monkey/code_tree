ba88687418b55f1cf310163ef8d7654e26b22f67
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-12-23 13:46:12 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 135ms, Memory: 48MB, Status: Passed - Codetree

--- 251223/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 251223/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -5,19 +5,32 @@ arr = [[0]*size for _ in range(size)]
 
 x1, y1, x2, y2 = map(int, input().split())
 
-for x in range(x1,x2+1):
-    for y in range(y1,y2+1):
+for x in range(x1,x2):
+    for y in range(y1,y2):
         arr[x + offset][y + offset] = 1
 
 x1, y1, x2, y2 = map(int, input().split())
 
-for x in range(x1,x2+1):
-    for y in range(y1,y2+1):
+for x in range(x1,x2):
+    for y in range(y1,y2):
         arr[x + offset][y + offset] = 0
 
-ans = 0
-for x in range(size):
-    for y in range(size):
-        ans += arr[x][y]
+min_x = size
+max_x = -1
+min_y = size
+max_y = -1
 
-print(ans)
+for i in range(size):
+    for j in range(size):
+        if arr[i][j] == 1:
+            if i < min_x: min_x = i
+            if i > max_x: max_x = i
+            if j < min_y: min_y = j
+            if j > max_y: max_y = j
+
+if max_x == -1:
+    print(0)
+else:
+    width = max_x - min_x + 1
+    height = max_y - min_y + 1
+    print(width * height)

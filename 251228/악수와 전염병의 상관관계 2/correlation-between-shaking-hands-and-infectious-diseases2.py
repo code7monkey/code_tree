1feb9baa5b373db14aad148b685f293f51ae1f1d
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-12-28 14:31:29 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251228/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 251228/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,28 +1,30 @@
 N, K, P, T = map(int, input().split())
 events = [tuple(map(int, input().split())) for _ in range(T)]
+events.sort(key=lambda x: x[0])
 
-infected = [0] * (N + 1)   # 1~N 사용
-cnt = [0] * (N + 1)        # 감염된 후 악수 카운트
+infected = [0] * (N + 1)  # 1~N
+cnt = [0] * (N + 1)       # 감염된 "이후" 악수 횟수
 
 infected[P] = 1
 
-events.sort(key=lambda x: x[0])  # 시간순
-
 for t, x, y in events:
-    # 현재 악수 시점에서 전염 가능한지(악수하기 "전" 상태로 판단)
-    x_can = infected[x] and cnt[x] < K
-    y_can = infected[y] and cnt[y] < K
+    pre_x = infected[x]
+    pre_y = infected[y]
 
-    # 전염 처리 (전염 가능자 -> 상대가 미감염이면 감염)
+    x_can = pre_x and cnt[x] < K
+    y_can = pre_y and cnt[y] < K
+
+    # 전염 (악수 직전 상태 기준으로만)
     if x_can and not infected[y]:
         infected[y] = 1
     if y_can and not infected[x]:
         infected[x] = 1
 
-    # 악수 카운트는 "감염자"면 증가 (감염자끼리도 포함)
-    if infected[x]:
+    # 악수 카운트는 "악수 직전 감염자"만 증가
+    if pre_x:
         cnt[x] += 1
-    if infected[y]:
+    if pre_y:
         cnt[y] += 1
 
 print(''.join(map(str, infected[1:])))
+

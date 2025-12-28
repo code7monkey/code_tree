da8f306bad2552455328321695933e1f57022a93
Lee Seung Hyun <155277312+code7monkey@users.noreply.github.com>
2025-12-28 10:25:59 +0900

    Title: 벌금은 누구에게, Time: 65ms, Memory: 16MB, Status: Passed - Codetree

--- 251228/벌금은 누구에게/who-will-pay.py
+++ 251228/벌금은 누구에게/who-will-pay.py
@@ -1,10 +1,16 @@
 n, m, k = map(int, input().split())
 lst = [int(input()) for _ in range(m)]
 
+printed = False
+
 pos = [0] * (n+1)
  
 for i in lst:
     pos[i] += 1
     if pos[i] >= k:
         print(i)
+        printed = True
         break
+
+if not printed:
+    print(-1) 
